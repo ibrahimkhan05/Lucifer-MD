c11fa4c93911fa13b5d3176140557ce322af36c8
Ibrahim khan <thewordlh95@gmail.com>
2024-11-06 21:31:42 +0500

    ASD

--- handler.js
+++ handler.js
@@ -1,3 +1,4 @@
+
 const { Function: Func, Scraper, Cooldown, Spam, InvCloud, Config: env } = new(require('@neoxr/wb'))
 const cron = require('node-cron')
 const cooldown = new Cooldown(env.cooldown)
@@ -194,7 +195,7 @@ module.exports = async (client, ctx) => {
                client.reply(m.chat, global.status.private, m)
                continue
             }
-            cmd.async(m, { client, args, text, isPrefix: prefix, prefixes, command, groupMetadata, participants, users, chats, groupSet, setting, isOwner, isAdmin, isBotAdmin, plugins, blockList, env, ctx, store, Func, Scraper })
+            cmd.async(m, { client, args, text, isPrefix: prefix, prefixes, command, groupMetadata, participants, users, chats, groupSet, setting, isOwner, isAdmin, isBotAdmin, plugins: Object.fromEntries(Object.entries(plugins).filter(([name, _]) => !setting.pluginDisable.includes(name))), blockList, env, ctx, store, Func, Scraper })
             break
          }
       } else {
@@ -226,7 +227,7 @@ module.exports = async (client, ctx) => {
             if (event.admin && !isAdmin) continue
             if (event.private && m.isGroup) continue
             if (event.download && (!setting.autodownload || (body && env.evaluate_chars.some(v => body.startsWith(v))))) continue
-            event.async(m, { client, body, prefixes, groupMetadata, participants, users, chats, groupSet, setting, isOwner, isAdmin, isBotAdmin, plugins, blockList, env, ctx, store, Func, Scraper })
+            event.async(m, { client, body, prefixes, groupMetadata, participants, users, chats, groupSet, setting, isOwner, isAdmin, isBotAdmin, plugins: Object.fromEntries(Object.entries(plugins).filter(([name, _]) => !setting.pluginDisable.includes(name))), blockList, env, ctx, store, Func, Scraper })
          }
       }
    } catch (e) {
