aaff7454e70d5ba05c349425086a54f8c5a3d1e7
Ibrahim khan <thewordlh95@gmail.com>
2024-11-08 20:35:20 +0500

    ASD

--- plugins/download/test.js
+++ plugins/download/test.js
@@ -7,31 +7,6 @@ exports.run = {
     async: async (m, { client, text, isPrefix, command, Func }) => {
         try {
             var buttons = [{
-                name: "quick_reply",
-                buttonParamsJson: JSON.stringify({
-                   display_text: "OWNER",
-                   id: '.owner'
-                }),
-             }, {
-                name: "cta_url",
-                buttonParamsJson: JSON.stringify({
-                   display_text: "Rest API",
-                   url: "https://api.neoxr.my.id",
-                   merchant_url: "https://api.neoxr.my.id"
-                })
-             }, {
-                name: "cta_copy",
-                buttonParamsJson: JSON.stringify({
-                   display_text: "Copy",
-                   copy_code: "123456"
-                })
-             }, {
-                name: "cta_call",
-                buttonParamsJson: JSON.stringify({
-                   display_text: "Call",
-                   phone_number: "6285887776722"
-                })
-             }, {
                 name: "single_select",
                 buttonParamsJson: JSON.stringify({
                    title: "Tap!",
@@ -57,48 +32,7 @@ exports.run = {
                 media: global.db.setting.cover // video or image link
              })
              
-            //  // carousel message
-            //  const cards = [{
-            //     header: {
-            //        imageMessage: global.db.setting.cover,
-            //        hasMediaAttachment: true,
-            //     },
-            //     body: {
-            //        text: "P"
-            //     },
-            //     nativeFlowMessage: {
-            //        buttons: [{
-            //           name: "cta_url",
-            //           buttonParamsJson: JSON.stringify({
-            //              display_text: 'Contact Owner',
-            //              url: 'https://api.neoxr.eu',
-            //              webview_presentation: null
-            //           })
-            //        }]
-            //     }
-            //  }, {
-            //     header: {
-            //        imageMessage: global.db.setting.cover,
-            //        hasMediaAttachment: true,
-            //     },
-            //     body: {
-            //        text: "P"
-            //     },
-            //     nativeFlowMessage: {
-            //        buttons: [{
-            //           name: "cta_url",
-            //           buttonParamsJson: JSON.stringify({
-            //              display_text: 'Contact Owner',
-            //              url: 'https://api.neoxr.eu',
-            //              webview_presentation: null
-            //           })
-            //        }]
-            //     }
-            //  }]
-             
-            //  client.sendCarousel(m.chat, cards, m, {
-            //     content: 'Hi!'
-            //  })
+            
              
 
         } catch (e) {
