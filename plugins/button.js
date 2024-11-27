901f3ebccb22b3285fca7e37b69f23ae87a44c43
Ibrahim khan <thewordlh95@gmail.com>
2024-11-27 19:07:33 +0500

    asdas

--- plugins/button.js
+++ plugins/button.js
@@ -13,42 +13,8 @@ exports.run = {
                display_text: "OWNER",
                id: '.owner'
             }),
-         }, {
-            name: "cta_url",
-            buttonParamsJson: JSON.stringify({
-               display_text: "Rest API",
-               url: "https://api.neoxr.my.id",
-               merchant_url: "https://api.neoxr.my.id"
-            })
-         }, {
-            name: "cta_copy",
-            buttonParamsJson: JSON.stringify({
-               display_text: "Copy",
-               copy_code: "123456"
-            })
-         }, {
-            name: "cta_call",
-            buttonParamsJson: JSON.stringify({
-               display_text: "Call",
-               phone_number: "6285887776722"
-            })
-         }, {
-            name: "single_select",
-            buttonParamsJson: JSON.stringify({
-               title: "Tap!",
-               sections: [{
-                  rows: [{
-                     title: "Owner",
-                     description: `X`,
-                     id: `.owner`
-                  }, {
-                     title: "Runtime",
-                     description: `Y`,
-                     id: `.run`
-                  }]
-               }]
-            })
-         }]
+         }, 
+         ]
          
          // button & list
          client.sendIAMessage(m.chat, buttons, m, {
