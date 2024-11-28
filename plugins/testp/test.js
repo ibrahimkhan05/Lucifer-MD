1ee9938250224d2f61ba1fb628b8af628d60daa1
Ibrahim khan <thewordlh95@gmail.com>
2024-11-28 19:25:52 +0500

    asda

--- plugins/testp/test.js
+++ plugins/testp/test.js
@@ -1,15 +1,49 @@
-const { Youtube } = require('@neoxr/youtube-scraper')
-const yt = new Youtube({
-   fileAsUrl: false
-})
 
 exports.run = {
     usage: ['session'],
     category: 'test',
     async: async (m, { client, text, body }) => {
-       // import { gpt } from "gpti";
-       yt.play('jana samjo na').then(console.log)
-
+      const cards = [{
+         header: {
+            imageMessage: global.db.setting.cover,
+            hasMediaAttachment: true,
+         },
+         body: {
+            text: "P"
+         },
+         nativeFlowMessage: {
+            buttons: [{
+               name: "cta_url",
+               buttonParamsJson: JSON.stringify({
+                  display_text: 'Contact Owner',
+                  url: 'https://api.neoxr.eu',
+                  webview_presentation: null
+               })
+            }]
+         }
+      }, {
+         header: {
+            imageMessage: global.db.setting.cover,
+            hasMediaAttachment: true,
+         },
+         body: {
+            text: "P"
+         },
+         nativeFlowMessage: {
+            buttons: [{
+               name: "cta_url",
+               buttonParamsJson: JSON.stringify({
+                  display_text: 'Contact Owner',
+                  url: 'https://api.neoxr.eu',
+                  webview_presentation: null
+               })
+            }]
+         }
+      }]
+      
+      client.sendCarousel(m.chat, cards, m, {
+         content: 'Hi!'
+      })
     },
     error: false
 };
