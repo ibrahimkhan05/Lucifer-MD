98bea69216e61c7089ecf531e88265e88d972ab3
Ibrahim khan <thewordlh95@gmail.com>
2024-11-28 18:41:01 +0500

    asdasdsa

--- plugins/refereal/refreal.js
+++ plugins/refereal/refreal.js
@@ -27,12 +27,12 @@ exports.run = {
                 global.db.users = global.db.users.map(v => v.jid === user.jid ? user : v);
                 
                 // Create a redeem link for the referral code
-                redeemLink = `https://api.whatsapp.com/send?phone=447375237255&text=/redeem-${user.referralCode}`;
+                redeemLink = `https://api.whatsapp.com/send?phone=447375237255&text=/redeem${user.referralCode}`; // Removed space after '/redeem'
                 
                 client.reply(m.chat, `Your referral code is: ${user.referralCode}\nRedeem it here: ${redeemLink}`, m);
              } else {
                 // If the user already has a referral code, show it
-                redeemLink = `https://api.whatsapp.com/send?phone=-447375237255&text=/redeem-${user.referralCode}`;
+                redeemLink = `https://api.whatsapp.com/send?phone=447375237255&text=/redeem${user.referralCode}`; // Removed space after '/redeem'
                 client.reply(m.chat, `Your referral code is already: ${user.referralCode}\nRedeem it here: ${redeemLink}`, m);
              }
           } else {
@@ -48,5 +48,4 @@ exports.run = {
     restrict: true,
     cache: true,
     location: __filename
- }
- 
+}
