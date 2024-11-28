b919c83ce133e2571c68b7743c84ea0df819d909
Ibrahim khan <thewordlh95@gmail.com>
2024-11-28 18:56:34 +0500

    asdas

--- plugins/refereal/myrefreal.js
+++ plugins/refereal/myrefreal.js
@@ -1,7 +1,7 @@
 exports.run = {
     usage: ['myreferral'],
     hidden: [''],
-    use: 'Shows how many users you have referred.',
+    use: 'Shows how many users you have referred and how much limit you have.',
     category: 'referral',
     async: async (m, {
        client,
@@ -16,17 +16,26 @@ exports.run = {
        try {
           // Find the user by their JID
           let user = global.db.users.find(v => v.jid === m.sender);
-
+ 
           if (!user) {
              return client.reply(m.chat, 'You are not registered, please try again later.', m);
           }
-
+ 
           // Check how many users the current user has referred
           let referredCount = user.referredUsers.length;
-
-          // Send the count to the user
-          client.reply(m.chat, `You have referred ${referredCount} user(s).`, m);
-
+ 
+          // Calculate the extra limit based on the number of referrals
+          let extraLimit = referredCount * 10; // 10 for each referral
+          if (user.referralUsed) {
+             extraLimit += 5; // Add 5 if the user has used a referral code
+          }
+ 
+          // Total limit is base limit plus extra limit
+          let totalLimit = env.limit + extraLimit;
+ 
+          // Send the referral count and total limit to the user
+          client.reply(m.chat, `You have referred ${referredCount} user(s). Your total limit is ${totalLimit}.`, m);
+ 
        } catch (e) {
           console.log(e);
           client.reply(m.chat, Func.jsonFormat(e), m);
@@ -37,4 +46,5 @@ exports.run = {
     restrict: true,
     cache: true,
     location: __filename
-}
+ }
+ 
