cad6751375a05578cb5be5d4f1bf8bd0cbdaaa6f
Ibrahim khan <thewordlh95@gmail.com>
2024-12-11 20:12:42 +0500

    asd

--- plugins/testp/test.js
+++ plugins/testp/test.js
@@ -1,10 +1,31 @@
-import { MessageType, MessageOptions, Mimetype } from '@whiskeysockets/baileys'
+
 exports.run = {
     usage: ['session'],
     category: 'test',
     async: async (m, { client, text, body }) => {
-
-    },
+      const cards = [{
+         header: {
+            imageMessage: global.db.setting.cover,
+            hasMediaAttachment: true,
+         },
+         body: {
+            text: "P"
+         },
+         nativeFlowMessage: {
+            buttons: [{
+               name: "quick_reply",
+               buttonParamsJson: JSON.stringify({
+                  display_text: "OWNER",
+                  id: ".owner"
+               })
+            }]
+         }
          
+      }, ]
+      
+      client.sendCarousel(m.chat, cards, m, {
+         content: 'Hi!'
+      })
+    },
     error: false
 };
