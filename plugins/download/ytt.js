320aee822d95353e7f8689317c9ecdb246e637fc
Ibrahim khan <thewordlh95@gmail.com>
2024-12-22 22:20:10 +0500

    sadas

---
+++ plugins/download/ytt.js
@@ -0,0 +1,30 @@
+
+const { Youtube } = require('@neoxr/youtube-scraper')
+const yt = new Youtube({
+   fileAsUrl: false
+})
+exports.run = {
+   usage: ['play'],
+   use: 'query',
+   category: 'downloader',
+   async: async (m, {
+      client,
+      text,
+      isPrefix,
+      command,
+      users,
+      env,
+      Func,
+      Scraper
+   }) => {
+      try {
+        yt.play('wide awake').then(console.log)
+      } catch (e) {
+         client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   error: false,
+   restrict: true,
+   cache: true,
+   location: __filename
+}
