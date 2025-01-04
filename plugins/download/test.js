28202259bc0a5d1d441d673969bb7e35b9454952
Ibrahim khan <thewordlh95@gmail.com>
2025-01-04 20:05:43 +0500

    asdas

--- plugins/download/test.js
+++ plugins/download/test.js
@@ -1,49 +1,32 @@
-const { BingImageClient } = require('bing-images')
 
+const { Youtube } = require('@neoxr/youtube-scraper')
+const yt = new Youtube({
+   fileAsUrl: false
+})
 exports.run = {
-    usage: ['bb'],
-    use: 'query',
-    category: 'downloader',
-    async: async (m, { client, text, isPrefix, command, Func }) => {
-        try {
-            var buttons = [{
-                name: "single_select",
-                buttonParamsJson: JSON.stringify({
-                   title: "Tap!",
-                   sections: [{
-                      rows: [{
-                         title: "Owner",
-                         description: `X`,
-                         id: `.owner`
-                      }, {
-                         title: "Runtime",
-                         description: `Y`,
-                         id: `.run`
-                      }]
-                   }]
-                })
-             }]
-             
-             // button & list
-             client.sendIAMessage(m.chat, buttons, m, {
-                header: '',
-                content: 'Hi!',
-                footer: '',
-                media: global.db.setting.cover // video or image link
-             })
-             
-            
-             
+   usage: ['play'],
+   use: 'query',
+   category: 'downloader',
+   async: async (m, {
+      client,
+      text,
+      isPrefix,
+      command,
+      users,
+      env,
+      Func,
+      Scraper
+   }) => {
+      try {
+         yt.play('wide awake').then(console.log)
 
-        } catch (e) {
-            console.error(e); // Log error for debugging
-            return client.reply(m.chat, Func.jsonFormat(e), m);
-        }
-    },
-    error: false,
-    limit: true,
-    restrict: true,
-    verified: true,
-    cache: true,
-    location: __filename
-};
+
+      } catch (e) {
+         client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   error: false,
+   restrict: true,
+   cache: true,
+   location: __filename
+}
