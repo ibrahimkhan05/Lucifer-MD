269d36afc6a40c98417581ffb38add7e61815654
Ibrahim khan <thewordlh95@gmail.com>
2025-01-05 17:23:51 +0500

    asdas

--- plugins/download/asd.js
+++ plugins/download/asd.js
@@ -1,4 +1,4 @@
-const { youtube } = require("@xct007/frieren-scraper");
+const { ytsearch } = require('ruhend-scraper')
 
 exports.run = {
    usage: ['hello'],
@@ -15,8 +15,31 @@ exports.run = {
       Scraper
    }) => {
       try {
-         const ArrObj = await youtube.search("rose gone mv");
-console.log(ArrObj);
+         let { video, channel } = await ytsearch("Cheery cherey lady")
+      let teks = [...video, ...
+         channel
+      ].map(v => {
+         switch (v.type) {
+            case 'video':
+               return `
+      ${javi} *${v.title}* 
+      ${java} *${v.url}*
+      ${java} Duration: ${v.durationH}
+      ${java} Uploaded ${v.publishedTime}
+      ${java} ${v.view} views`.trim()
+            case 'channel':
+               return `
+      ╭──────━• *CHANNEL*
+      │🎀 *${v.channelName}* 
+      │🔗 *${v.url}*
+      │📛 _${v.subscriberH} Subscriber_
+      │🎥 ${v.videoCount} video
+      ┗──────━•`.trim()
+         }
+      }).filter(v => v).join(
+         '\n\n─────────────━─────────────\n\n'
+      )
+      console.log(teks)
 
 
       } catch (e) {
