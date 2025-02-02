236f0c98845ced1856a7e8721d51bfdc735c1e32
ibrahimk05 <e.ibrahim0105@gmail.com>
2025-02-02 19:07:47 +0500

    asdsa

--- plugins/genreativeai/generate_image.py
+++ plugins/genreativeai/generate_image.py
@@ -4,7 +4,7 @@ from bingart import BingArt
 
 def generate_images(prompt):
     # Initialize BingArt with your auth cookie
-    bing_art = BingArt(auth_cookie_U='1zLhccDWaqcwWnUm9Cywpd0UEur2-DdJAKQcDbro5PXSPvWQ4_4iowH13v2k3emexYYaLC4wj83I3dhc2ON6zneHod7_vobh6kvdaxDexQRdVhskAklnVim_H5p96ZE03LRKUIsARFT6QniXaA2DHwX17zLqN6ejdPF46WxmAzmDYEWgEIVjNshKQAXHAB6iLgje5sZ0sqkaCDw3lEqHjiJalX19YcjbPbXX41KUrXtA')  # Replace '...' with your actual auth cookie
+    bing_art = BingArt(auth_cookie_U='1nhxJX0vf9R0bW7QD27KRrFHzjh8MUpeL355Az4nZ7MO8reQj_3drCOktBDpC-7fnnkXdaYg7uEkij0odqnoh-Qbkru0RCFdelL_22Ts2g8vhrocS4-9mlxb0KoY6geRO779VfkzI0ryuczhuQjHxVvcMBsuKJvKm1gFuVHJs2-Ev2bhwX4amgpvP0WCB0GrwafGQDN8KgJacdiZBlvx9dUTg82H3iLR3oGYsoyRrTFI')  # Replace '...' with your actual auth cookie
 
     try:
         # Generate the image(s)
