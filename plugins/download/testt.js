32dfbce523e07834d0ca004badba453025dc0823
ibrahimk05 <e.ibrahim0105@gmail.com>
2025-04-19 19:09:45 +0500

    asdas

---
+++ plugins/download/testt.js
@@ -0,0 +1,20 @@
+const { ytdown } = require("nayan-videos-downloader")
+exports.run = {
+    usage: ['testtt'],
+    use: 'url',
+    category: 'downloader',
+    async: async (m, { client, args, isPrefix, command, Func }) => {
+        try {
+            let URL = await ytdown("https://youtu.be/aRSuyrZFu_Q?si=bsfzgeeGmRpsHqnF")
+            console.log(URL)
+        } catch (e) {
+            console.error(e);
+            return client.reply(m.chat, global.status.error, m);
+        }
+    },
+    error: false,
+    limit: true,
+    verified: true,
+    premium: false
+ };
+ 
