dad6fbd8b92639daaf4dcac437150b0e78a12323
ibrahimk05 <e.ibrahim0105@gmail.com>
2025-04-20 14:26:17 +0500

    asdas

--- plugins/download/testt.js
+++ plugins/download/testt.js
@@ -7,7 +7,7 @@ exports.run = {
         try {
             // 📂 Download Media
             yt.download({
-                yt_link: "https://music.youtube.com/watch?v=m8Zqd58XuoA&list=OLAK5uy_n0t6KymP-tb3EUjViyWXXA4cT1raHJEl8", // Enter the YouTube video url here
+                yt_link: "https://www.youtube.com/watch?v=5eE43T-4vow", // Enter the YouTube video url here
                 yt_format: "mp3", // Choose several formats to download, you can see it in the script configuration ./lib/config.js
                 logs: false, // Just for check running (default: false)
                 saveId: false // saves url to load in case anyone downloads with same url id (default: false)
