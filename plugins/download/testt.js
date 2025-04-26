f762a1bbc9701bac1ed360204b3dba6f934f7b8b
ibrahimk05 <e.ibrahim0105@gmail.com>
2025-04-26 18:58:55 +0500

    asdas

--- plugins/download/testt.js
+++ plugins/download/testt.js
@@ -5,25 +5,18 @@ exports.run = {
     category: 'downloader',
     async: async (m, { client, args, isPrefix, command, Func }) => {
         try {
-            // url YouTube kamu
-            const url = 'https://www.youtube.com/watch?v=YOUR_VIDEO_ID';
+            const url = 'https://youtu.be/8of5w7RgcTc?list=RD8of5w7RgcTc';
 
-            /*
-             * quality download
-             * pilih di Quality Available
-             * bisa dalam format audio
-             * maupun video
-             */
-
-            const quality = 128
-
+            // quality download, pilih di Quality Available
+            const quality = "128"
+            
             /* 
              * atau kamu bisa langsung url
-             * saja untuk default quality (128 & 360)
-             * example: ytdlv2(url)
+             * saja untuk default quality (128)
+             * example: ytmp3(url)
             */
-
-            ytdlv2(url, quality)
+            
+            ytmp3(url, quality)
                 .then(result => {
                     if (result.status) {
                         console.log('Download Link:', result.download);
