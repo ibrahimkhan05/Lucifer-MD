3273e24a7f61d8bd3417d929ae96b7fd669ced25
ibrahimk05 <e.ibrahim0105@gmail.com>
2025-05-01 16:13:18 +0500

    asdasdas

--- plugins/download/testt.js
+++ plugins/download/testt.js
@@ -6,10 +6,19 @@ exports.run = {
     async: async (m, { client, args, isPrefix, command, Func }) => {
         try {
 
-            https.request('https://cdn306.savetube.su/download-direct/video/360/43650049619893a6aae7b5262e5fbf695b3ccdd3', { method: 'HEAD' }, res => {
-                const size = res.headers['content-length'];
-                console.log('Size in MB:', (size / 1024 / 1024).toFixed(2));
-              }).end();
+            const url = 'https://cdn306.savetube.su/download-direct/video/360/43650049619893a6aae7b5262e5fbf695b3ccdd3';
+
+https.request(url, {
+  method: 'HEAD',
+  headers: { 'User-Agent': 'Mozilla/5.0' }
+}, res => {
+  const size = res.headers['content-length'];
+  if (size) {
+    console.log('Size in MB:', (size / 1024 / 1024).toFixed(2));
+  } else {
+    console.log('Content-Length header not found');
+  }
+}).end();
         } catch (e) {
             console.error(e);
             return client.reply(m.chat, 'An error occurred while processing your request.', m);
