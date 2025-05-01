b90343ba2651de068c1d3145c3e0cf27cb3df65f
ibrahimk05 <e.ibrahim0105@gmail.com>
2025-05-01 16:08:54 +0500

    asdas

--- plugins/download/testt.js
+++ plugins/download/testt.js
@@ -1,18 +1,31 @@
-const { Youtube } = require('@neoxr/youtube-scraper')
-const yt = new Youtube({
-   fileAsUrl: false,
-   proxy: {
-      host: 'sg.xzcdn.live',
-      port: 29841
-   }
-})
+const { search, ytmp3, ytmp4, ytdlv2, channel } = require('@vreden/youtube_scraper');
 exports.run = {
     usage: ['testtt'],
     use: 'youtube video URL',
     category: 'downloader',
     async: async (m, { client, args, isPrefix, command, Func }) => {
         try {
-            yt.play('wide awake').then(console.log)
+
+            const url = 'https://youtu.be/60ItHLz5WEA';
+
+            // quality download, pilih di Quality Available
+            const quality = "360"
+
+            /* 
+             * atau kamu bisa langsung url
+             * saja untuk default quality (360)
+             * example: ytmp4(url)
+            */
+
+            ytmp4(url, quality)
+                .then(result => {
+                    if (result.status) {
+                        console.log('Download Link:', result.download);
+                        console.log('Metadata:', result.metadata);
+                    } else {
+                        console.error('Error:', result.result);
+                    }
+                });
         } catch (e) {
             console.error(e);
             return client.reply(m.chat, 'An error occurred while processing your request.', m);
